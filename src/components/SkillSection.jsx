f62f81ac7e2beb06626b706166d1892d6b88e069
Richi-Kodehode <richard25sfj@gmail.com>
2025-03-05 13:24:47 +0100

    added structure and data

---
+++ src/components/SkillSection.jsx
@@ -0,0 +1,7 @@
+export default function SkillSection() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
---
+++ src/components/data/data.js
@@ -0,0 +1,14 @@
+export const skillset = [
+  {
+    id: 1,
+    src: "/public/react.svg",
+  },
+  {
+    id: 2,
+    src: "/public/vite.svg",
+  },
+  {
+    id: 3,
+    src: "/public/javascript.svg",
+  },
+];
